2c1ef4ea77d64d0d9a5b9a673c9e6b71719e4352
Ilya <soloviev1097@gmail.com>
2018-12-29 12:37:27 +0300

    исправил компоненты

---
+++ src/constans/tableHeaders.js
@@ -0,0 +1,20 @@
+export const personHeaders=[
+
+            {
+                'cellID':'firstName',
+                'headerContent':'Фамилия'
+            },
+            {
+              'cellID':'name',
+              'headerContent':'Имя'
+            },
+            {
+                'cellID':'patronymic',
+                'headerContent':'Отчество'
+            },
+            {
+                'cellID':'telephone',
+                'headerContent':'Телефон'
+            }
+        ]
+
